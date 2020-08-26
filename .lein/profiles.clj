56f0df4515e40f5bea061510dc899453f7dc24e1
Kalle Norrestam <kalle.norrestam@empear.com>
2020-08-26 23:17:18 +0200

    add

---
+++ .lein/profiles.clj
@@ -0,0 +1,14 @@
+{:user {:plugins [;[lein-localrepo "0.5.4"]
+                  [lein-pprint  "1.2.0"]]}
+ :vim [:dev
+       {:plugins [[cider/cider-nrepl  "0.22.1"]
+                  [venantius/ultra  "0.6.0"]]} ]
+ :code [:dev
+        {:dependencies [[nrepl "0.6.0"]]
+         :plugins [[cider/cider-nrepl "0.22.1"]
+                   [venantius/ultra "0.6.0"]]}]
+ :rebl [:dev 
+        {:resource-paths  ["/Users/kalle/REBL/REBL-0.9.220/REBL-0.9.220.jar"]
+         :dependencies  [[org.clojure/core.async  "0.4.490"]
+                         [cljfmt  "0.6.4"]]}]
+ }
