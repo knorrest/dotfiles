cc5acfa7ee20951884af020f4a3d027725147674
Kalle Norrestam <kalle.norrestam@empear.com>
2021-01-25 09:01:44 +0100

    Update lein

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,11 +1,12 @@
 {:user {:plugins [;[lein-localrepo "0.5.4"]
-                  [lein-pprint  "1.2.0"]]}
+                  [lein-pprint  "1.2.0"]
+                  [lein-cloverage  "1.1.2"]]}
  :vim [:dev
-       {:plugins [[cider/cider-nrepl  "0.22.1"]
+       {:plugins [[cider/cider-nrepl  "0.25.5"]
                   [venantius/ultra  "0.6.0"]]} ]
  :code [:dev
         {:dependencies [[nrepl "0.6.0"]]
-         :plugins [[cider/cider-nrepl "0.22.1"]
+         :plugins [[cider/cider-nrepl "0.25.5"]
                    [venantius/ultra "0.6.0"]]}]
  :rebl [:dev 
         {:resource-paths  ["/Users/kalle/REBL/REBL-0.9.220/REBL-0.9.220.jar"]
