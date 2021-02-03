75ef3f0545e54730b1450c887fa32608c946c1e8
Kalle Norrestam <kalle.norrestam@empear.com>
2021-02-03 23:16:53 +0100

    Remove ultra

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -6,8 +6,7 @@
                   [venantius/ultra  "0.6.0"]]} ]
  :code [:dev
         {:dependencies [[nrepl "0.6.0"]]
-         :plugins [[cider/cider-nrepl "0.25.5"]
-                   [venantius/ultra "0.6.0"]]}]
+         :plugins [[cider/cider-nrepl "0.25.5"]]}]
  :rebl [:dev 
         {:resource-paths  ["/Users/kalle/REBL/REBL-0.9.220/REBL-0.9.220.jar"]
          :dependencies  [[org.clojure/core.async  "0.4.490"]
